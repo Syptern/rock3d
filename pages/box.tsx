6c43315c2cf0df47b68baabab3be106259f98b40
Syptern <timonvanzoest@gmail.com>
2021-12-10 18:55:55 +0100

    first commit

---
+++ pages/box.tsx
@@ -0,0 +1,30 @@
+import { Environment, OrbitControls } from "@react-three/drei"
+import { Canvas, useFrame, useLoader } from "@react-three/fiber"
+import { Suspense, useRef } from "react"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+const Box = () => {
+  const gltf = useLoader(GLTFLoader, "./mesh1/textured.gltf")
+
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  )
+}
+
+const CanvasRTF = () => {
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Box />
+          <OrbitControls />
+          <Environment preset="sunset" />
+        </Suspense>
+      </Canvas>
+    </div>
+  )
+}
+
+export default CanvasRTF
