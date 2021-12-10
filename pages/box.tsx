d6c16c4c02095ee7d0723dd685f6f2d5adc4c3f3
Syptern <timonvanzoest@gmail.com>
2021-12-10 22:24:06 +0100

    click to place hold

--- pages/box.tsx
+++ pages/box.tsx
@@ -1,24 +1,58 @@
 import { Environment, OrbitControls } from "@react-three/drei"
 import { Canvas, useFrame, useLoader } from "@react-three/fiber"
-import { Suspense, useRef } from "react"
+import { Suspense, useRef, useState } from "react"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader"
 
-const Box = () => {
-  const gltf = useLoader(GLTFLoader, "./test/textured.gltf")
+const Ball = ({
+  position,
+}: {
+  position: { x: number; y: number; z: number }
+}) => (
+  <mesh
+    visible
+    userData={{ hello: "world" }}
+    position={[position.x, position.y, position.z]}
+    rotation={[Math.PI / 2, 0, 0]}
+  >
+    <sphereGeometry args={[0.02, 10, 10]} />
+    <meshStandardMaterial color="hotpink" transparent />
+  </mesh>
+)
+
+const Box = ({ handleClick }: any) => {
+  const gltf = useLoader(GLTFLoader, "./test/modelDraco.gltf", (loader) => {
+    const dracoLoader = new DRACOLoader()
+    dracoLoader.setDecoderPath("/test/draco-gltf/")
+    loader.setDRACOLoader(dracoLoader)
+  })
 
   return (
     <>
-      <primitive object={gltf.scene} scale={0.4} />
+      <primitive
+        onDoubleClick={(e: any) => handleClick(e)}
+        object={gltf.scene}
+        scale={0.4}
+      />
     </>
   )
 }
 
 const CanvasRTF = () => {
+  const [balls, setBalls] = useState<any>([{ x: 1, y: 1, z: 1 }])
+  const handleClick = (e: any) => {
+    console.log(e)
+    setBalls([...balls, { x: e.point.x, y: e.point.y, z: e.point.z }])
+  }
+
   return (
     <div className="App">
       <Canvas>
         <Suspense fallback={null}>
-          <Box />
+          <Box handleClick={handleClick} />
+          {balls.map((e: any, i: number) => (
+            <Ball position={e} key={i} />
+          ))}
           <OrbitControls />
           <Environment preset="sunset" />
         </Suspense>
